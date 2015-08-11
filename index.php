1fddd0054236059ef3dff8fdd8208d5e1ef8e866
analyn <analyncajocson@ymail.com>
2015-08-11 11:11:21 +0800

    used class

--- index.php
+++ index.php
@@ -2,5 +2,11 @@
 <?php
 
 	$stefan = new person();
+	$jimy = new person;
+	$stefan->set_name("Stefan Mischook");
+	$jimy->set_name("Nick Waddles");
+
+	echo "Stefan's full name: " . $stefan->get_name();
+	echo "Nick's full name: " . $jimy->get_name();
 
 ?>
