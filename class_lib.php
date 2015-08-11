ba788173d4e7d7dce14bb858ea885cd5bfa9ec8d
analyn <analyncajocson@ymail.com>
2015-08-11 16:05:15 +0800

    clean class_lip.php code

--- class_lib.php
+++ class_lib.php
@@ -1,15 +1,12 @@
 <?
 	class person {
 		var $name;
-		public $height;
-		protected $social_insurance;
-		private $pinn_number;
 
 		function __construct($persons_name) {
 			$this->set_name($persons_name);
 		}
-		private function get_pinn_number(){
-			return $this->pinn_number;;
+		function get_namer(){
+			return $this->name;
 		}
 	
 		protected function set_name($new_name){
