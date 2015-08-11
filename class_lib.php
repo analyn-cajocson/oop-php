be28cb68b2b1901afb0dc939e9215acff58b6a4d
analyn <analyncajocson@ymail.com>
2015-08-11 15:03:53 +0800

    class_lib.php

--- class_lib.php
+++ class_lib.php
@@ -8,11 +8,8 @@
 		function __construct($persons_name) {
 			$this->name = $persons_name;
 		}
-		function set_name($new_name){
-			$this->name = $new_name;
-		}
-		function get_name(){
-			return $this->name;
+		function get_pinn_number(){
+			return $this->pinn_number;;
 		}
 	
 
