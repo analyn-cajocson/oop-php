0e4f8d0929f3c625a9808ba62e9dcb052570ec78
analyn <analyncajocson@ymail.com>
2015-08-11 14:37:40 +0800

    used construct instead of set_name

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php include("class_lib.php"); ?>
 <?php
 
-	$stefan = new person();
-	$jimy = new person;
-	$stefan->set_name("Stefan Mischook");
-	$jimy->set_name("Nick Waddles");
-
+	$stefan = new person("Stefan Mischook");
 	echo "Stefan's full name: " . $stefan->get_name();
-	echo "Nick's full name: " . $jimy->get_name();
 
 ?>
